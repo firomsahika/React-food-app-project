0cb01092d03294cd98503f57c968ead15d7f4fe7
Firomsa Hika <firomsahika2022@gmail.com>
2024-04-14 16:55:20 -0700

    Fixing some styling of Review page

--- src/pages/FoodApp/FoodApp.jsx
+++ src/pages/FoodApp/FoodApp.jsx
@@ -5,10 +5,12 @@ import app from "../../assets/app/Frame 54.png"
 import iphone from "../../assets/app/Group 7.png"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { FaGooglePlay } from 'react-icons/fa6'
 
 
 
 const FoodApp = () => {
+  
   useEffect(() => {
     AOS.init({
       duration: 1500
@@ -17,12 +19,19 @@ const FoodApp = () => {
 
 
   return (
-    <section className='items-center justify-center font-poppins'>
-       <div className='relative flex flex-col items-center justify-center px-6 lg:flex '>
-         <img
-         className='lg:h-[60vh] lg:flex hidden'
-         src={app} />
-         
+    <section className='items-center max-h-screen justify-center font-poppins'>
+       <div 
+       data-aos="fade-up"
+       className='relative flex flex-col items-center justify-center px-6 lg:flex lg:py-20'>
+         <div className='bg-black hidden lg:flex h-[50vh] w-[70rem] rounded-2xl relative '>
+
+          <div className='flex '>
+          <FaGooglePlay size={50}  className=' absolute left-32 top-40  text-amber-500'/>
+          <p className='absolute left-28 top-56  text-white'>Play Store</p>
+          <FaAppStoreIos size={50} className='absolute left-52 top-40 text-amber-500'/>
+          <p className='font-poppins absolute left-52 top-56  text-white'>Apple Store</p>
+          </div>
+         </div>
          <div className='flex flex-col items-center justify-center '>
          <p className='text-3xl font-bold text-center lg:hidden lg:5xl md:4xl'>Download ios & Android App</p>
          <div className='flex gap-4 py-4 lg:hidden'>
@@ -34,8 +43,8 @@ const FoodApp = () => {
          size={40}/>
          </div>
             <img
-            data-aos="fade-up"
-            className='lg:absolute p-0 w-[20rem] lg:w-[50rem]  lg:left-[24rem] lg:top-[-12rem] '
+            
+            className='lg:absolute p-0 w-[20rem] lg:w-[50rem]  lg:left-[25rem] lg:top-[-9rem] '
             src={iphone} alt="group 7" />
          </div>
        </div>
